4d09d2b8b59a53b7acc10628e356491374f28614
Thomas Martinez <tmartinez114@gmail.com>
2021-12-08 10:30:14 -0500

    added style

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap/dist/js/bootstrap";
-import React, { useState, useEffect } from "react";
-import Search from "./components/Search/Search"
-import Card from "./components/Card/Card"
-import Pagination from "./components/Pagination/Pagination"
-import Filter from "./components/Filter/Filter"
-import Navbar from "./components/Navbar/Navbar"
+// import React, { useState, useEffect } from "react";
+// import Search from "./components/Search/Search"
+// import Card from "./components/Card/Card"
+// import Pagination from "./components/Pagination/Pagination"
+// import Filter from "./components/Filter/Filter"
+// import Navbar from "./components/Navbar/Navbar"
 
 function App() {
   return (
