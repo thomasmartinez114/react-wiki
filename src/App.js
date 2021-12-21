37c63cd263e2caadb6c4b99e89aed907255deae8
Thomas Martinez <tmartinez114@gmail.com>
2021-12-20 22:00:34 -0500

    started from beginning

--- src/App.js
+++ src/App.js
@@ -1,42 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap/dist/js/bootstrap";
-import React, { useState, useEffect } from "react";
-import Search from "./components/Search/Search"
-import Card from "./components/Card/Card"
-import Pagination from "./components/Pagination/Pagination"
-import Filter from "./components/Filter/Filter"
-import Navbar from "./components/Navbar/Navbar"
-let api = `https://rickandmortyapi.com/api/character/?page=1`;
+// import React, { useState, useEffect } from "react";
+// import Search from "./components/Search/Search"
+// import Card from "./components/Card/Card"
+// import Pagination from "./components/Pagination/Pagination"
+// import Filter from "./components/Filter/Filter"
+// import Navbar from "./components/Navbar/Navbar"
+// let api = `https://rickandmortyapi.com/api/character/?page=1`;
 
 
 function App() {
-  let [fetchedData, updateFetchedData] = useState([]);
-  let { info, results} = fetchedData;
-
-  useEffect(() => {
-    (async function () {
-      let data = await fetch(api).then((res) => res.json());
-      // console.log(data);
-      updateFetchedData(data);
-    })();
-  }, [api]);
-}
-
-  return (
-    <div className="App">
-      <h1 className="text-center mb-3">Characters</h1>
-      <div className="container">
-        <div className="row">
-          Filter component will be placed here
-          <div className="col-lg-8 col-12">
-            <Card results={results} />
-          </div>
-        </div>
+    return (
+      <div className="App">
+        <h1 className="text-center">Rick & Morty <span className="text-primary">WiKi</span> </h1>
       </div>
-    </div>
-  );
-
+    );
+  }
 
 export default App;
