83d4af2e46cf69fb125e4ea8ee992428418dd832
Thomas Martinez <tmartinez114@gmail.com>
2021-12-20 22:17:33 -0500

    added style and margin to title with my-4

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ import "bootstrap/dist/js/bootstrap";
 function App() {
     return (
       <div className="App">
-        <h1 className="text-center">Rick & Morty <span className="text-primary">WiKi</span> </h1>
+        <h1 className="text-center ubuntu my-4">Rick & Morty <span className="text-primary">WiKi</span> </h1>
       </div>
     );
   }
