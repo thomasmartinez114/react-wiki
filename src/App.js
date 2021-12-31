cf335378ee48782671b4d1675fb7647c2deda958
Thomas Martinez <tmartinez114@gmail.com>
2021-12-31 00:22:08 -0500

    fetching data from api - logging (data.results) and info showing in console of browser

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
 import './App.css';
+import React, { useState, useEffect } from 'react'
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap/dist/js/bootstrap";
-// import React, { useState, useEffect } from "react";
-// import Search from "./components/Search/Search"
-// import Card from "./components/Card/Card"
-// import Pagination from "./components/Pagination/Pagination"
 import Filters from './components/Filters/Filters';
 import Cards from './components/Cards/Cards';
-// import Navbar from "./components/Navbar/Navbar"
-// let api = `https://rickandmortyapi.com/api/character/?page=1`;
 
 
 function App() {
+
+  // this will bind to the pagination - setPageNumber will be the function to change page
+  let [pageNumber, setPageNumber] = useState(1);
+  let api = `https://rickandmortyapi.com/api/character/?page=${pageNumber}`;
+
+  useEffect(() => {
+    
+    // IIFE (Immediately Invoked Func. Exp.)
+    (async function() {
+      let data = await fetch(api).then(res=>res.json())
+      console.log(data.results);
+    })()
+
+  }, [api])
+
     return (
       <div className="App">
         <h1 className="text-center ubuntu my-4">Rick & Morty <span className="text-primary">WiKi</span> </h1>
