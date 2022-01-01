3ac2f38eeb9890c5deb0ad16a71dc66913c5bb83
Thomas Martinez <tmartinez114@gmail.com>
2022-01-01 15:11:38 -0500

    resolved refresh page crash

--- src/components/Pagination/Pagination.js
+++ src/components/Pagination/Pagination.js
@@ -4,7 +4,7 @@ import React from 'react';
 import ReactPaginate from 'react-paginate';
 
 const Pagination = ({ info, pageNumber, setPageNumber }) => {
-  return <ReactPaginate pageCount={info.pages}/>;
+  return <ReactPaginate pageCount={info?.pages}/>;
 };
 
 export default Pagination;
