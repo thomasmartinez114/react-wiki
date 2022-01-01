a73253e4ee163f4c056dd16ccd8aed730686768b
Thomas Martinez <tmartinez114@gmail.com>
2022-01-01 14:30:58 -0500

    got pagination to show 1-3 and 38-42 on page but it is breaking on refresh

--- src/App.js
+++ src/App.js
@@ -49,7 +49,7 @@ function App() {
         </div>
       </div>
 
-      <Pagination pageNumber={pageNumber} setPageNumber={setPageNumber} />
+      <Pagination info={info} pageNumber={pageNumber} setPageNumber={setPageNumber} />
     </div>
   );
 }
--- src/components/Pagination/Pagination.js
+++ src/components/Pagination/Pagination.js
@@ -3,28 +3,8 @@
 import React from 'react';
 import ReactPaginate from 'react-paginate';
 
-const Pagination = ({ pageNumber, setPageNumber }) => {
-  let next = () => {
-    setPageNumber(x => x + 1); // page count will increment by 1
-  };
-
-  let prev = () => {
-    if (pageNumber === 1) return;
-    setPageNumber(x => x - 1);
-  };
-
-  return (
-    <div>
-      <div className='container d-flex justify-content-center gap-5 my-5'>
-        <button onClick={prev} className='btn btn-primary'>
-          Prev
-        </button>
-        <button onClick={next} className='btn btn-primary'>
-          Next
-        </button>
-      </div>
-    </div>
-  );
+const Pagination = ({ info, pageNumber, setPageNumber }) => {
+  return <ReactPaginate pageCount={info.pages}/>;
 };
 
 export default Pagination;
