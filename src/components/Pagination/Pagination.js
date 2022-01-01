ca28e2472be4ef99d9de55336e4ae1d329d3e7ee
Thomas Martinez <tmartinez114@gmail.com>
2022-01-01 18:00:09 -0500

    added style to the pagination buttons

--- src/components/Pagination/Pagination.js
+++ src/components/Pagination/Pagination.js
@@ -5,7 +5,17 @@ import ReactPaginate from 'react-paginate';
 
 const Pagination = ({ info, pageNumber, setPageNumber }) => {
   // if info exist, then show the pages
-  return <ReactPaginate pageCount={info?.pages} />;
+  return (
+  <ReactPaginate
+      className="pagination justify-content-center gap-4 my-4"
+      nextLabel="Next"
+      previousLabel="Prev"
+      nextClassName="btn btn-primary"
+      previousClassName='btn btn-primary'
+      pageClassName='page-item'
+      pageLinkClassName='page-link'
+      pageCount={info?.pages} />
+  )
 };
 
 export default Pagination;
