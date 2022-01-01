858b2d95bb4e3979232cdb8ad5f9f033165f71ce
Thomas Martinez <tmartinez114@gmail.com>
2022-01-01 13:37:00 -0500

    added Search bar styling

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -4,9 +4,13 @@ import styles from './Search.module.scss';
 
 export const Search = () => {
   return (
-    <form className=''>
-      <input placeholder='Search for Characters' type='text' className='' />
-      <button className='btn btn-primary'>Search</button>
+    <form className='d-flex justify-content-center gap-4 mb-5'>
+      <input
+        placeholder='Search for Characters'
+        type='text'
+        className={styles.input}
+      />
+      <button className={`${styles.btn} btn btn-primary fs-5`}>Search</button>
     </form>
   );
 };
