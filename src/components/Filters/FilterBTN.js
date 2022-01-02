aff5846e897b46fe0dd6cc086620a73dcfc593bf
Thomas Martinez <tmartinez114@gmail.com>
2022-01-01 23:57:23 -0500

    button toggle functional now and applied into Status

--- src/components/Filters/FilterBTN.js
+++ src/components/Filters/FilterBTN.js
@@ -1,22 +1,33 @@
-import React from 'react'
+import React from 'react';
 
 const FilterBTN = () => {
-    return (
-        <div>
-<div class="form-check">
-  <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1"/>
-  <label class="form-check-label" for="flexRadioDefault1">
-    Default radio
-  </label>
-</div>
-<div class="form-check">
-  <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" checked/>
-  <label class="form-check-label" for="flexRadioDefault2">
-    Default checked radio
-  </label>
-            </div>
-            </div>
-    )
-}
+  return (
+    <div>
+      <div class='form-check'>
+        <input
+          class='form-check-input'
+          type='radio'
+          name='flexRadioDefault'
+          id='flexRadioDefault1'
+        />
+        <label class='btn btn-outline-primary' for='flexRadioDefault1'>
+          Single toggle
+        </label>
+      </div>
+      <div class='form-check'>
+        <input
+          class='form-check-input'
+          type='radio'
+          name='flexRadioDefault'
+          id='flexRadioDefault2'
+          checked
+        />
+        <label class='btn btn-outline-primary' for='flexRadioDefault2'>
+          Default checked radio
+        </label>
+      </div>
+    </div>
+  );
+};
 
-export default FilterBTN
+export default FilterBTN;
