500fbc19474befe9f5838398ad944b023f6ded21
Thomas Martinez <tmartinez114@gmail.com>
2022-01-02 00:08:18 -0500

    Status accordion rendering the statuses within the array

--- src/components/Filters/Category/Status.js
+++ src/components/Filters/Category/Status.js
@@ -1,21 +1,36 @@
-import React from 'react'
+import React from 'react';
 import FilterBTN from '../FilterBTN';
 
 const Status = () => {
-    return (
-        <div className="accordion-item">
-    <h2 class="accordion-header" id="headingThree">
-      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-        Status
-      </button>
-    </h2>
-    <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
-                <div className="accordion-body">
-                    <FilterBTN />
+  let status = ['Alive', 'Dead', 'Unknown'];
+  return (
+    <div className='accordion-item'>
+      <h2 class='accordion-header' id='headingThree'>
+        <button
+          class='accordion-button collapsed'
+          type='button'
+          data-bs-toggle='collapse'
+          data-bs-target='#collapseThree'
+          aria-expanded='false'
+          aria-controls='collapseThree'
+        >
+          Status
+        </button>
+      </h2>
+      <div
+        id='collapseThree'
+        class='accordion-collapse collapse'
+        aria-labelledby='headingThree'
+        data-bs-parent='#accordionExample'
+      >
+        <div className='accordion-body'>
+          {status.map((items, index) => (
+            <FilterBTN key={index} name='status' index={index} items={items} />
+          ))}
+        </div>
       </div>
     </div>
-  </div>
-    )
-}
+  );
+};
 
-export default Status
+export default Status;
--- src/components/Filters/FilterBTN.js
+++ src/components/Filters/FilterBTN.js
@@ -1,29 +1,17 @@
 import React from 'react';
 
-const FilterBTN = () => {
+const FilterBTN = ({ name, index, items }) => {
   return (
     <div>
       <div class='form-check'>
         <input
           class='form-check-input'
           type='radio'
-          name='flexRadioDefault'
-          id='flexRadioDefault1'
+          name={name}
+          id={`${name}-${index}`}
         />
-        <label class='btn btn-outline-primary' for='flexRadioDefault1'>
-          Single toggle
-        </label>
-      </div>
-      <div class='form-check'>
-        <input
-          class='form-check-input'
-          type='radio'
-          name='flexRadioDefault'
-          id='flexRadioDefault2'
-          checked
-        />
-        <label class='btn btn-outline-primary' for='flexRadioDefault2'>
-          Default checked radio
+        <label class='btn btn-outline-primary' for={`${name}-${index}`}>
+          {items}
         </label>
       </div>
     </div>
