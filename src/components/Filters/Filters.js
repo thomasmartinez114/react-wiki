d1bb4957436c5865bb82484b7abd417b9514212c
Thomas Martinez <tmartinez114@gmail.com>
2022-01-01 23:41:59 -0500

    added Filters within their own components and added to Filters component

--- src/components/Filters/Filters.js
+++ src/components/Filters/Filters.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import Gender from './Category/Gender';
+import Species from './Category/Species';
+import Status from './Category/Status';
 
 const Filters = () => {
   return (
@@ -6,9 +9,14 @@ const Filters = () => {
       <div className='text-center fw-bold fs-4 mb-2'>Filter</div>
       <div
         style={{ cursor: 'pointer' }}
-        className='text-center text-primary text-decoration-underline'
+        className='text-center text-primary text-decoration-underline mb-4'
       >
         Clear Filters
+      </div>
+      <div className="accordion" id="accordionExample">
+        <Status />
+        <Species />
+        <Gender />
       </div>
     </div>
   );
