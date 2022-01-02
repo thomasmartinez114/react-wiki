c5845f7e501e1787adef356393ec83886cb3eb43
Thomas Martinez <tmartinez114@gmail.com>
2022-01-02 16:52:24 -0500

    Clear Filters - functional and refreshes page

--- src/components/Filters/Filters.js
+++ src/components/Filters/Filters.js
@@ -4,10 +4,20 @@ import Species from './Category/Species';
 import Status from './Category/Status';
 
 const Filters = ({ setStatus, setPageNumber, setGender, setSpecies }) => {
+  // Clear function to set all variables to empty and refresh page
+  let clear = () => {
+    setStatus('');
+    setPageNumber('');
+    setGender('');
+    setSpecies('');
+    window.location.reload(false);
+  };
+
   return (
     <div className='col-3'>
       <div className='text-center fw-bold fs-4 mb-2'>Filter</div>
       <div
+        onClick={clear}
         style={{ cursor: 'pointer' }}
         className='text-center text-primary text-decoration-underline mb-4'
       >
