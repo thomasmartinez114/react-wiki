4f53bf5f7e907a5a7a90f3f9c2fd4cf73ed1ef4e
Thomas Martinez <tmartinez114@gmail.com>
2022-01-01 23:26:49 -0500

    init Filter/ and Gender,Species,Status and styled the Filter and Clear Filter

--- src/components/Filters/Filters.js
+++ src/components/Filters/Filters.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from 'react';
 
 const Filters = () => {
-    return (
-        <div>Filters</div>
-    )
-}
+  return (
+    <div className='col-3'>
+      <div className='text-center fw-bold fs-4 mb-2'>Filter</div>
+      <div
+        style={{ cursor: 'pointer' }}
+        className='text-center text-primary text-decoration-underline'
+      >
+        Clear Filters
+      </div>
+    </div>
+  );
+};
 
-export default Filters
+export default Filters;
