03aecb7d03093c74a9d169e6169efa3462e13108
Thomas Martinez <tmartinez114@gmail.com>
2022-01-02 13:39:27 -0500

    mapped through SPecies and added to the accordion

--- src/components/Filters/Category/Species.js
+++ src/components/Filters/Category/Species.js
@@ -1,20 +1,49 @@
-import React from 'react'
+import React from 'react';
+import FilterBTN from '../FilterBTN';
 
 const Species = () => {
-    return (
-        <div className="accordion-item">
-    <h2 class="accordion-header" id="headingTwo">
-      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-        Species
-      </button>
-    </h2>
-    <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
-      <div className="accordion-body">
+  let species = [
+    'Human',
+    'Alien',
+    'Humanoid',
+    'Poppybutthole',
+    'Mythological',
+    'Unknown',
+    'Animal',
+    'Disease',
+    'Robot',
+    'Cronenberg',
+    'Planet',
+  ];
 
+  return (
+    <div className='accordion-item'>
+      <h2 class='accordion-header' id='headingTwo'>
+        <button
+          class='accordion-button collapsed'
+          type='button'
+          data-bs-toggle='collapse'
+          data-bs-target='#collapseTwo'
+          aria-expanded='false'
+          aria-controls='collapseTwo'
+        >
+          Species
+        </button>
+      </h2>
+      <div
+        id='collapseTwo'
+        class='accordion-collapse collapse'
+        aria-labelledby='headingTwo'
+        data-bs-parent='#accordionExample'
+      >
+        <div className='accordion-body'>
+          {species.map((items, index) => (
+            <FilterBTN key={index} name='species' index={index} items={items} />
+          ))}
+        </div>
       </div>
     </div>
-  </div>
-    )
-}
+  );
+};
 
-export default Species
+export default Species;
