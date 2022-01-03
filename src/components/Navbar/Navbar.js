59e59768b69aedc4d06a6be11307f12f46f5c9aa
Thomas Martinez <tmartinez114@gmail.com>
2022-01-02 22:32:50 -0500

    styled the navbar pages

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -19,14 +19,14 @@ const Navbar = () => {
         <span class='navbar-toggler-icon'></span>
       </button>
       <div class='collapse navbar-collapse justify-content-end' id='navbarNav'>
-        <div className='navbar-nav'>
-          <NavLink to='/' class='nav-link'>
+        <div className='navbar-nav fs-5'>
+          <NavLink to='/' className='nav-link'>
             Characters
           </NavLink>
-          <NavLink to='/episodes' class='nav-link'>
+          <NavLink to='/episodes' className='nav-link'>
             Episodes
           </NavLink>
-          <NavLink to='/location' class='nav-link'>
+          <NavLink to='/location' className='nav-link'>
             Location
           </NavLink>
         </div>
