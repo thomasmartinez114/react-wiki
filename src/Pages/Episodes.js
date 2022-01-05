9a0d1ea1e7231bb855ca67b96a42bbb6886d57e6
Thomas Martinez <tmartinez114@gmail.com>
2022-01-04 22:47:12 -0500

    init Episodes/ and api pull for the Episodes -- showing in the browser console

--- src/Pages/Episodes.js
+++ src/Pages/Episodes.js
@@ -1,6 +1,15 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Episodes = () => {
+  let [id, setID] = useState(1);
+  let api = `https://rickandmortyapi.com/api/episode/${id}`;
+
+  useEffect(() => {
+    (async function () {
+      let data = await fetch(api).then(res => res.json());
+      console.log(data);
+    })();
+  }, [api]);
   return <div>The episodes are here.</div>;
 };
 
