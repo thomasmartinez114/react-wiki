2167411f9cee5e293f833996827dc0b65939cb91
Thomas Martinez <tmartinez114@gmail.com>
2022-01-04 22:50:41 -0500

    created info, setInfo and applied that data from api to info

--- src/Pages/Episodes.js
+++ src/Pages/Episodes.js
@@ -2,12 +2,14 @@ import React, { useState, useEffect } from 'react';
 
 const Episodes = () => {
   let [id, setID] = useState(1);
+  let [info, setInfo] = useState([]);
+  console.log(info);
   let api = `https://rickandmortyapi.com/api/episode/${id}`;
 
   useEffect(() => {
     (async function () {
       let data = await fetch(api).then(res => res.json());
-      console.log(data);
+      setInfo(data);
     })();
   }, [api]);
   return <div>The episodes are here.</div>;
