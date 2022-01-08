74e89e0aef3854ae8be7a54c5041f8e4a01b6ece
Thomas Martinez <tmartinez114@gmail.com>
2022-01-07 20:37:34 -0500

    Input Group - have the episodes rendering through the selector and adding 1 to avoid starting from episode 0

--- src/components/Filters/Category/InputGroup.js
+++ src/components/Filters/Category/InputGroup.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
 const InputGroup = ({ total }) => {
-  console.log([...Array(total).keys()]);
   return (
     <div class='input-group mb-3'>
       <select class='form-select' id='inputGroupSelect01'>
         <option selected>Choose...</option>
+        {[...Array(total).keys()].map(episodeNumber => {
+          return <option value={episodeNumber + 1}>{episodeNumber + 1}</option>;
+        })}
       </select>
     </div>
   );
