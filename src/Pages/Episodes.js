e752be25e3510d0a28ad72fe618c6c1248684efa
Thomas Martinez <tmartinez114@gmail.com>
2022-01-07 20:27:18 -0500

    Input Group - started init

--- src/Pages/Episodes.js
+++ src/Pages/Episodes.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import Cards from '../components/Cards/Cards';
+import InputGroup from '../components/Filters/Category/InputGroup';
 
 const Episodes = () => {
   let [id, setID] = useState(1);
@@ -36,7 +37,10 @@ const Episodes = () => {
         </h5>
       </div>
       <div className='row'>
-        <div className='col-3'>Pick Episodes</div>
+        <div className='col-3'>
+          <h4 className='text-center mb-4'>Pick Episodes</h4>
+          <InputGroup />
+        </div>
         <div className='col-8'>
           <div className='row'>
             <Cards results={results} />
