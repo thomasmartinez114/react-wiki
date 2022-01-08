8c8de392fcb1587ca990d6f11f101418d6727803
Thomas Martinez <tmartinez114@gmail.com>
2022-01-07 20:39:18 -0500

    Input Group - added listing to have Episode - {episode number}

--- src/components/Filters/Category/InputGroup.js
+++ src/components/Filters/Category/InputGroup.js
@@ -1,12 +1,16 @@
 import React from 'react';
 
-const InputGroup = ({ total }) => {
+const InputGroup = ({ total, name, setID }) => {
   return (
     <div class='input-group mb-3'>
       <select class='form-select' id='inputGroupSelect01'>
         <option selected>Choose...</option>
         {[...Array(total).keys()].map(episodeNumber => {
-          return <option value={episodeNumber + 1}>{episodeNumber + 1}</option>;
+          return (
+            <option value={episodeNumber + 1}>
+              {name} - {episodeNumber + 1}
+            </option>
+          );
         })}
       </select>
     </div>
