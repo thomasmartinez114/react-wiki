85c598fb7b577038e44d2c055c1a40a4e8743d5e
Thomas Martinez <tmartinez114@gmail.com>
2022-01-07 20:34:34 -0500

    Input Group - fixed drop down selector

--- src/components/Filters/Category/InputGroup.js
+++ src/components/Filters/Category/InputGroup.js
@@ -4,12 +4,8 @@ const InputGroup = ({ total }) => {
   console.log([...Array(total).keys()]);
   return (
     <div class='input-group mb-3'>
-      <div class='input-group-prepend'> </div>
-      <select class='custom-select' id='inputGroupSelect01'>
+      <select class='form-select' id='inputGroupSelect01'>
         <option selected>Choose...</option>
-        <option value='1'>One</option>
-        <option value='2'>Two</option>
-        <option value='3'>Three</option>
       </select>
     </div>
   );
