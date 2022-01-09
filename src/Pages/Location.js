f419a1253103f197b67acb83815f10600c22b7b8
Thomas Martinez <tmartinez114@gmail.com>
2022-01-09 00:14:32 -0500

    location page rendering data

--- src/Pages/Location.js
+++ src/Pages/Location.js
@@ -1,7 +1,54 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Cards from '../components/Cards/Cards';
+import InputGroup from '../components/Filters/Category/InputGroup';
 
 const Location = () => {
-  return <div>The locations are here.</div>;
+  let [id, setID] = useState(1);
+  let [info, setInfo] = useState([]);
+  // results should match what we used in App.js
+  let [results, setResults] = useState([]);
+  let { name, type, dimension } = info;
+  let api = `https://rickandmortyapi.com/api/location/${id}`;
+
+  useEffect(() => {
+    (async function () {
+      let data = await fetch(api).then(res => res.json());
+      setInfo(data);
+
+      // pull characters from episode
+      let locations = await Promise.all(
+        data.residents.map(characterLocations => {
+          return fetch(characterLocations).then(res => res.json());
+        })
+      );
+      setResults(locations);
+    })();
+  }, [api]);
+  return (
+    <div className='container'>
+      <div className='row'>
+        <h1 className='text-center mb-2'>
+          {/* if name is blank then print Uknown, if not, put the Name */}
+          Episode:{' '}
+          <span className='text-primary'>{name === '' ? 'Unknown' : name}</span>
+        </h1>
+        <h5 className='text-center mb-4'>
+          {/* Air Date {air_date === '' ? 'Unknown' : air_date} */}
+        </h5>
+      </div>
+      <div className='row'>
+        <div className='col-3'>
+          <h4 className='text-center mb-4'>Pick Episodes</h4>
+          <InputGroup setID={setID} name='Episode' total={51} />
+        </div>
+        <div className='col-8'>
+          <div className='row'>
+            <Cards results={results} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Location;
