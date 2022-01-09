62302ffabc22ff52dfd1dca64d98379b4189cde3
Thomas Martinez <tmartinez114@gmail.com>
2022-01-09 00:46:19 -0500

    added api call to the CardDetails

--- src/components/Cards/CardDetails.js
+++ src/components/Cards/CardDetails.js
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 
 const CardDetails = () => {
   let { id } = useParams();
+  let [fetchedData, updateFetchedData] = useState([]);
+  console.log(fetchedData);
+
+  let api = `https://rickandmortyapi.com/api/character/${id}`;
+  useEffect(() => {
+    // IIFE (Immediately Invoked Func. Exp.)
+    (async function () {
+      let data = await fetch(api).then(res => res.json());
+      // console.log(data.results);
+      updateFetchedData(data);
+    })();
+  }, [api]);
+
   return <div>The details of the card are here - {id}</div>;
 };
 
