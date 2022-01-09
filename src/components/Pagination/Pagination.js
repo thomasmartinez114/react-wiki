922c3a566bfcbce49425b29b3c1a7167a33afe44
Thomas Martinez <tmartinez114@gmail.com>
2022-01-09 01:28:56 -0500

    pagination responsive

--- src/components/Pagination/Pagination.js
+++ src/components/Pagination/Pagination.js
@@ -15,25 +15,42 @@ const Pagination = ({ info, pageNumber, setPageNumber }) => {
 
   useEffect(() => {
     window.addEventListener('resize', updateDimension);
-    return () => window.addEventListener('resize', updateDimension);
+    return () => window.removeEventListener('resize', updateDimension);
   }, []);
 
   return (
-    <ReactPaginate
-      className='pagination justify-content-center gap-4 my-4'
-      forcePage={pageNumber === 1 ? 0 : pageNumber - 1}
-      nextLabel='Next >'
-      previousLabel='< Prev'
-      nextClassName='btn'
-      previousClassName='btn'
-      pageClassName='page-item'
-      pageLinkClassName='page-link'
-      activeClassName='active'
-      onPageChange={data => {
-        setPageNumber(data.selected + 1);
-      }}
-      pageCount={info?.pages}
-    />
+    <>
+      <style jsx>
+        {`
+          @media (max-width: 768px) {
+            .next,
+            .previous {
+              display: none;
+            }
+            .pagination {
+              font-size: 14px;
+            }
+          }
+        `}
+      </style>
+      <ReactPaginate
+        className='pagination justify-content-center gap-4 my-4'
+        forcePage={pageNumber === 1 ? 0 : pageNumber - 1}
+        nextLabel='Next >'
+        previousLabel='< Prev'
+        nextClassName='btn next'
+        previousClassName='btn previous'
+        pageClassName='page-item'
+        pageLinkClassName='page-link'
+        marginPagesDisplayed={width < 576 ? 1 : 2}
+        pageRangeDisplayed={width < 576 ? 1 : 2}
+        activeClassName='active'
+        onPageChange={data => {
+          setPageNumber(data.selected + 1);
+        }}
+        pageCount={info?.pages}
+      />
+    </>
   );
 };
 
