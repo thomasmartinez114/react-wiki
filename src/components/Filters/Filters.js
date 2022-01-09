6064648aef85b88e779b0af8fb4c16b7ea1e0948
Thomas Martinez <tmartinez114@gmail.com>
2022-01-09 01:11:28 -0500

    Filter responsive

--- src/components/Filters/Filters.js
+++ src/components/Filters/Filters.js
@@ -14,7 +14,7 @@ const Filters = ({ setStatus, setPageNumber, setGender, setSpecies }) => {
   };
 
   return (
-    <div className='col-3'>
+    <div className='col-lg-3 col-12 mb-5'>
       <div className='text-center fw-bold fs-4 mb-2'>Filter</div>
       <div
         onClick={clear}
