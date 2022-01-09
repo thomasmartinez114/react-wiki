70f748958e631398b333d605d30d25c734b9d22f
Thomas Martinez <tmartinez114@gmail.com>
2022-01-09 00:30:36 -0500

    have the id being able to be used on episodes/id and location/id or just on home page

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import Navbar from './components/Navbar/Navbar';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Episodes from './Pages/Episodes';
 import Location from './Pages/Location';
+import CardDetails from './components/Cards/CardDetails';
 
 function App() {
   return (
@@ -21,8 +22,12 @@ function App() {
 
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/:id' element={<CardDetails />} />
+
         <Route path='/episodes' element={<Episodes />} />
+        <Route path='/episodes/:id' element={<CardDetails />} />
         <Route path='/location' element={<Location />} />
+        <Route path='/location/:id' element={<CardDetails />} />
       </Routes>
     </Router>
   );
