e8c13b960058954b97e92c7b9d06f4aa5ca9a9ef
Thomas Martinez <tmartinez114@gmail.com>
2022-01-09 01:22:13 -0500

    Pagination responvie -- made width of page update live so page knows how to responsive the pagination

--- src/components/Pagination/Pagination.js
+++ src/components/Pagination/Pagination.js
@@ -1,26 +1,40 @@
 // rfac
 
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactPaginate from 'react-paginate';
 
 const Pagination = ({ info, pageNumber, setPageNumber }) => {
   // if info exist, then show the pages
+  let [width, setwidth] = useState(window.innerWidth);
+
+  let updateDimension = () => {
+    setwidth(window.innerWidth);
+  };
+
+  console.log(width);
+
+  useEffect(() => {
+    window.addEventListener('resize', updateDimension);
+    return () => window.addEventListener('resize', updateDimension);
+  }, []);
+
   return (
-  <ReactPaginate
-      className="pagination justify-content-center gap-4 my-4"
-      forcePage={pageNumber===1? 0 : pageNumber - 1}
-      nextLabel="Next >"
-      previousLabel="< Prev"
-      nextClassName="btn"
+    <ReactPaginate
+      className='pagination justify-content-center gap-4 my-4'
+      forcePage={pageNumber === 1 ? 0 : pageNumber - 1}
+      nextLabel='Next >'
+      previousLabel='< Prev'
+      nextClassName='btn'
       previousClassName='btn'
       pageClassName='page-item'
       pageLinkClassName='page-link'
       activeClassName='active'
-      onPageChange={(data) => {
+      onPageChange={data => {
         setPageNumber(data.selected + 1);
       }}
-      pageCount={info?.pages} />
-  )
+      pageCount={info?.pages}
+    />
+  );
 };
 
 export default Pagination;
