346eaca912eafa56986b7bd52da3326b4d4490f9
Thomas Martinez <tmartinez114@gmail.com>
2022-01-09 00:05:25 -0500

    fetch the data when episode is selected

--- src/components/Filters/Category/InputGroup.js
+++ src/components/Filters/Category/InputGroup.js
@@ -3,7 +3,11 @@ import React from 'react';
 const InputGroup = ({ total, name, setID }) => {
   return (
     <div class='input-group mb-3'>
-      <select class='form-select' id='inputGroupSelect01'>
+      <select
+        onChange={e => setID(e.target.value)}
+        class='form-select'
+        id={name}
+      >
         <option selected>Choose...</option>
         {[...Array(total).keys()].map(episodeNumber => {
           return (
